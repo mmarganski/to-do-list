1e97d956dd36850484094e3f9bcee903f30365a3
mmarganski <mikolaj.marganski@gmail.com>
2021-07-07 14:34:38 +0200

    changed to object-based todo list

--- index.js
+++ index.js
@@ -1,197 +1,240 @@
-let ToDoObjectsArray = []
+class App {
 
-document.getElementById('mainSubmitButton').setAttribute('onclick', createNewTodo)
-document.getElementById('clearAllButton').setAttribute('onclick', clearAllTasks)
+    #todos
 
-class toDoObject{
+    constructor() {
+        this.todos = new Todos(() => this.printItems())
+        document.getElementById('mainSubmitButton').setAttribute('onclick', 'myApp.createNewTodo()')
+        document.getElementById('clearAllButton').setAttribute('onclick', 'myApp.clearAllTasks()')
+    }
+
+    clearAllTasks = () => {
+        if (confirm("Are you sure you want to clear every task?")) {
+            this.todos.clearAllTasks()
+            this.printItems()
+            document.getElementById("clearAllButton").setAttribute('hidden', true)
+        }
+    }
+
+    createNewTodo = () => {
+        const inputValue = document.getElementById('mainInput').value
+
+        if(inputValue.length > 0) {
+            this.todos.createNewTodo(inputValue.substring(0, 140))
+            this.printItems()
+            document.getElementById("mainInput").value = ""
+            document.getElementById("clearAllButton").removeAttribute('hidden')
+        }
+    }
+
+    printItems() {
+        document.getElementById('debugger').innerText = window.localStorage.getItem('todos')
+    }
+}
+
+class Todos {
+
+    #toDoObjectsMap
+
+    constructor( onChange) {
+        this.toDoObjectsMap = new Map()
+        this.onChange = onChange
+
+        window.localStorage.setItem('todos', '')
+    }
+
+    createNewTodo = text => {
+        const newTodo = new Todo(text)
+
+        this.toDoObjectsMap.set(newTodo.id, newTodo)
+        this.createNewListElement(text, newTodo.id)
+        this.setDebuggerStrings()
+        this.onChange()
+    }
+
+    clearAllTasks = () => {
+        const allTasks = document.querySelectorAll("#toDoList li")
+
+        this.toDoObjectsMap.clear()
+        allTasks.forEach(this.deleteTodo)
+    }
+
+    deleteTodo = currentListElement => {
+        const id = currentListElement.getAttribute('id')
+
+        this.toDoObjectsMap.delete(id)
+        currentListElement.remove()
+        this.setDebuggerStrings()
+        this.onChange()
+
+        if(this.toDoObjectsMap.keys().length === 0) {
+            document.getElementById("clearAllButton").setAttribute('hidden', true)
+        }
+    }
+
+    showOptions = div => div.removeAttribute('hidden')
+
+    hideOptions = div => div.setAttribute('hidden', true)
+
+    hideButtons = currentListElement => currentListElement
+        .querySelectorAll('button')
+        .forEach(button => button.setAttribute('hidden', true))
+
+    showElement = (name, div) => {
+        const [elementToShow] = div.getElementsByClassName(name)
+
+        elementToShow.removeAttribute('hidden')
+    }
+
+    hideElement = (name, div) => {
+        const [elementToHide] = div.getElementsByClassName(name)
+
+        elementToHide.setAttribute('hidden', true)
+    }
+
+    editTodo = currentListElement => {
+        this.hideButtons(currentListElement)
+        this.hideElement('checkBox', currentListElement)
+        this.hideElement('textField', currentListElement)
+        this.showElement('inputField', currentListElement)
+        this.showElement('saveEditButton', currentListElement)
+    }
+
+    editTodoText = currentListElement => {
+        this.hideButtons(currentListElement)
+        this.hideElement('inputField', currentListElement)
+        this.showElement('editButton', currentListElement)
+        this.showElement('deleteButton', currentListElement)
+        this.showElement('checkBox', currentListElement)
+        this.showElement('textField', currentListElement)
+
+        const id = currentListElement.getAttribute('id')
+        const [inputFieldText] = currentListElement.getElementsByClassName('inputField')
+        const editedTodo = this.toDoObjectsMap.get(id)
+
+        if(inputFieldText.value.length > 0) {
+            currentListElement.querySelector('span').innerText = inputFieldText.value
+            editedTodo.changeText(inputFieldText.value)
+            this.setDebuggerStrings()
+            this.onChange()
+        }
+    }
+
+    toggleTodo = currentListElement => {
+        const [checkBox] = currentListElement.getElementsByClassName('checkBox')
+        const [toDoText] = currentListElement.getElementsByClassName('textField')
+        const id = currentListElement.getAttribute('id')
+        const toggledTodo = this.toDoObjectsMap.get(id)
+
+        if(!checkBox.checked) {
+
+            this.showElement('editButton', currentListElement)
+            this.showElement('deleteButton', currentListElement)
+            toDoText .style.setProperty('text-decoration', '')
+            toggledTodo.toggle()
+            this.setDebuggerStrings()
+            this.onChange()
+
+            return
+        }
+
+        this.hideButtons(currentListElement)
+        toDoText.style.setProperty('text-decoration', 'line-through')
+        toggledTodo.toggle()
+        this.setDebuggerStrings()
+        this.onChange()
+    }
+
+    createNewListElement = (text, id) => {
+
+        const checkbox = document.createElement('input')
+        checkbox.setAttribute('type', 'checkbox')
+        checkbox.setAttribute('class', 'checkBox')
+        checkbox.addEventListener('change', () => this.toggleTodo(li))
+
+        const inputField = document.createElement('input')
+        inputField.setAttribute('hidden', true)
+        inputField.setAttribute('class', 'inputField')
+        inputField.value = text
+
+        const editButton = document.createElement('button')
+        editButton.setAttribute('class', 'editButton')
+        editButton.addEventListener('click', () => this.editTodo(li))
+        editButton.textContent = 'edit'
+
+        const saveEditButton = document.createElement('button')
+        saveEditButton.setAttribute('hidden', true)
+        saveEditButton.setAttribute('class', 'saveEditButton')
+        saveEditButton.addEventListener('click', () => this.editTodoText(li))
+        saveEditButton.textContent = 'save'
+
+        const deleteButton = document.createElement('button')
+        deleteButton.setAttribute('class', 'deleteButton')
+        deleteButton.addEventListener('click', () => this.deleteTodo(li))
+        deleteButton.textContent = 'delete'
+
+        const textField = document.createElement('span')
+        textField.setAttribute('class', 'textField')
+        textField.innerText = text
+
+        const div = document.createElement('div')
+        div.setAttribute('class', 'toDoItem')
+        div.appendChild(checkbox)
+        div.appendChild(inputField)
+        div.appendChild(saveEditButton)
+        div.appendChild(editButton)
+        div.appendChild(deleteButton)
+        div.setAttribute('hidden', true)
+
+        const li = document.createElement('li')
+        li.setAttribute('id', id)
+        li.appendChild(div)
+        li.appendChild(textField)
+        li.addEventListener('mouseover', () => this.showOptions(div))
+        li.addEventListener('mouseout', () => {
+            if(inputField.hidden) {
+                this.hideOptions(div)
+            }
+        })
+
+        document.getElementById('toDoList').appendChild(li)
+    }
+
+    setDebuggerStrings = () => {
+        const strings = []
+
+        this.toDoObjectsMap.forEach( todo => strings.push(todo.toString()))
+        window.localStorage.setItem('todos', strings.join(' \r\n '))
+    }
+}
+
+class Todo {
+
+    #id
+    #isDone
+    #text
+
     constructor(input) {
         this.id = this.#createID()
         this.isDone = false
         this.text = input
     }
 
-    #createID() {
+    #createID = () => {
         return String(Math.random().toFixed(6) * 1000000)
     }
 
-    toggle() {
+    toggle = () => {
         this.isDone = !this.isDone
     }
 
-    changeText(inputText) {
+    changeText = inputText => {
         this.text = inputText
     }
-}
 
-const clearAllTasks = () => {
-    if (confirm("Are you sure you want to clear every task?")) {
-        const allTasks = document.querySelectorAll("#toDoList li")
-
-        ToDoObjectsArray = []
-        allTasks.forEach(deleteTodo)
+    toString = () => {
+        return `[${this.isDone ? "х" : " "}] ${this.text}`
     }
 }
 
-const createNewTodo = () => {
-    const inputValue = document.getElementById('mainInput').value
-
-    if(inputValue.length > 0) {
-        const newToDoObject = new toDoObject(inputValue)
-
-        document.getElementById("mainInput").value = ""
-        ToDoObjectsArray.push([newToDoObject.id, newToDoObject])
-        newListElement(inputValue, newToDoObject.id)
-        document.getElementById("clearAllButton").removeAttribute('hidden')
-        printItems()
-    }
-}
-
-const editTodo = currentListElement => {
-    hideButtons(currentListElement)
-    hideElement('checkBox', currentListElement)
-    hideElement('textField', currentListElement)
-    showElement('inputField', currentListElement)
-    showElement('saveEditButton',currentListElement)
-}
-
-const editTodoText = currentListElement => {
-    hideButtons(currentListElement)
-    hideElement('inputField', currentListElement)
-    showElement('editButton', currentListElement)
-    showElement('deleteButton', currentListElement)
-    showElement('checkBox', currentListElement)
-    showElement('textField', currentListElement)
-
-    const currentObject = getObjectFromDOMElement(currentListElement)
-    const [inputFieldText] = currentListElement.getElementsByClassName('inputField')
-
-    currentListElement.querySelector('span').innerText = inputFieldText.value
-    currentObject.changeText(inputFieldText.value)
-    printItems()
-}
-
-const getObjectFromDOMElement = currentListElement => {
-    const id = currentListElement.getAttribute('id')
-    const toDoObjectElement = ToDoObjectsArray.find(([elementId]) => elementId === id)
-
-    if(toDoObjectElement) {
-        const [, toDoObject] = toDoObjectElement
-
-        return toDoObject
-    }
-
-    return null
-}
-
-const deleteTodo = currentListElement => {
-    const id = currentListElement.getAttribute('id')
-
-    ToDoObjectsArray.filter(([elementId]) => elementId !== id)
-    currentListElement.remove()
-
-    if(ToDoObjectsArray.length > 0) {
-        document.getElementById("clearAllButton").setAttribute('hidden', true)
-    }
-
-    printItems()
-}
-
-const toggleTodo = currentListElement => {
-    const currentObject = getObjectFromDOMElement(currentListElement)
-    const [checkBox] = currentListElement.getElementsByClassName('checkBox')
-    const [toDoText] = currentListElement.getElementsByClassName('textField')
-
-    if(!checkBox.checked) {
-
-        showElement('editButton', currentListElement)
-        showElement('deleteButton', currentListElement)
-        toDoText .style.setProperty('text-decoration', '');
-        currentObject.toggle()
-        printItems()
-
-        return
-    }
-
-    hideButtons(currentListElement)
-    toDoText.style.setProperty('text-decoration', 'line-through')
-    currentObject.toggle()
-    printItems()
-}
-
-const hideElement = (name, div) => {
-    const [elementToHide] = div.getElementsByClassName(name)
-
-    elementToHide.setAttribute('hidden', true)
-}
-
-const showElement = (name, div) => {
-    const [elementToShow] = div.getElementsByClassName(name)
-
-    elementToShow.removeAttribute('hidden')
-}
-
-const printItems = () => document.getElementById('debugger').innerText = JSON.stringify([...ToDoObjectsArray.entries()])
-
-const hideButtons = currentListElement => currentListElement
-    .querySelectorAll('button')
-    .forEach(button => button.setAttribute('hidden', true))
-
-const hideOptions = div => div.setAttribute('hidden', true)
-
-const showOptions = div => div.removeAttribute('hidden')
-
-const newListElement = (text, id) => {
-
-    const checkbox = document.createElement('input')
-    checkbox.setAttribute('type', 'checkbox')
-    checkbox.setAttribute('class', 'checkBox')
-    checkbox.addEventListener('change', () => toggleTodo(li))
-
-    const inputField = document.createElement('input')
-    inputField.setAttribute('hidden', true)
-    inputField.setAttribute('class', 'inputField')
-    inputField.value = text
-
-    const editButton = document.createElement('button')
-    editButton.setAttribute('class', 'editButton')
-    editButton.addEventListener('click', () => editTodo(li))
-    editButton.textContent = 'edit'
-
-    const saveEditButton = document.createElement('button')
-    saveEditButton.setAttribute('hidden', true)
-    saveEditButton.setAttribute('class', 'saveEditButton')
-    saveEditButton.addEventListener('click', () => editTodoText(li))
-    saveEditButton.textContent = 'save'
-
-    const deleteButton = document.createElement('button')
-    deleteButton.setAttribute('class', 'deleteButton')
-    deleteButton.addEventListener('click', () => deleteTodo(li))
-    deleteButton.textContent = 'delete'
-
-    const textField = document.createElement('span')
-    textField.setAttribute('class', 'textField')
-    textField.innerText = text
-
-    const div = document.createElement('div')
-    div.setAttribute('class', 'toDoItem')
-    div.appendChild(checkbox)
-    div.appendChild(inputField)
-    div.appendChild(saveEditButton)
-    div.appendChild(editButton)
-    div.appendChild(deleteButton)
-    div.setAttribute('hidden', true)
-
-    const li = document.createElement('li')
-    li.setAttribute('id', id)
-    li.appendChild(div)
-    li.appendChild(textField)
-    li.addEventListener('mouseover', () => showOptions(div))
-    li.addEventListener('mouseout', () => {
-        if(inputField.hidden) {
-            hideOptions(div)
-        }
-    })
-
-    document.getElementById('toDoList').appendChild(li)
-
-}
+const myApp = new App()
