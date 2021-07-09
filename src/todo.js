7ab14250b3acb97ed9a208ae4a6eea6c07533a89
mmarganski <mikolaj.marganski@gmail.com>
2021-07-09 11:21:19 +0200

    converted to jQuery and split into multiple files

---
+++ src/todo.js
@@ -0,0 +1,32 @@
+class Todo {
+
+    id
+    #isDone
+    #text
+
+    constructor(input) {
+        this.id = this.#createID()
+        this.#isDone = false
+        this.#text = typeof input === "string"
+            ? input
+            : ""
+    }
+
+    #createID = () => {
+        return String(Math.random().toFixed(6) * 1000000)
+    }
+
+    toggle = () => {
+        this.#isDone = !this.#isDone
+    }
+
+    changeText = inputText => {
+        this.#text = inputText
+    }
+
+    toString = () => {
+        return `[${this.#isDone ? "x" : " "}] ${this.#text}`
+    }
+}
+
+export default Todo
